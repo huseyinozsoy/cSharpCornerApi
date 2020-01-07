d6de76bb76d67c5b0902ed6617f6311e5b17ff0c
huseyinozsoy <huseyin_ozsoy_87@hotmail.com>
2020-01-07 03:37:50 +0300

    cSharpCornerApi

---
+++ csharpcornerauthorionicapi/CsharpDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharpcornerauthorionicapi
+{
+    public class CsharpDbContext:DbContext
+    {
+        public CsharpDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ csharpcornerauthorionicapi/Migrations/20191005133252_initialmigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace csharpcornerauthorionicapi.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true),
+                    location = table.Column<string>(nullable: true),
+                    joineddate = table.Column<string>(nullable: true),
+                    biodata = table.Column<string>(nullable: true),
+                    intro = table.Column<string>(nullable: true),
+                    profilepicture = table.Column<string>(nullable: true),
+                    profilelink = table.Column<string>(nullable: true),
+                    role = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
